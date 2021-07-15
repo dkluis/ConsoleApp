3216cd3043038f276c8d6d45621ed200ac05bfee
dkluis <dkluis@icloud.com>
2021-07-15 17:08:55 -0400

    Started working on text file IO

---
+++ FileIO/FileIO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class FileIO
+    {
+        static void Main4()
+        {
+        }
+
+        public class TextFile
+        {
+            public bool WriteText(string[] line, bool appending)
+            {
+                // File.WriteAllLines("Config.txt", line, append: appending);
+                return true;
+            }
+
+            public string[] ReadLines()
+            {
+                String[] text = File.ReadAllLines("Config.txt");
+                return text;
+            }
+        }
+    }
+}
